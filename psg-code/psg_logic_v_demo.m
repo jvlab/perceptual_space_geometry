1bc3e31aad77744deb0b263daddcc873203bd3be
Jonathan D Victor <jdvicto@med.cornell.edu>
2023-08-29 17:46:46 -0400

    Add files via upload
    calculates partition logic and compares with manuscript

--- psg-code/psg_logic_v_demo.m
+++ psg-code/psg_logic_v_demo.m
@@ -19,7 +19,7 @@ corresp.symtent.nc=6;
 corresp.addtree.type='exclude_addtree_trans';
 corresp.addtree.nc=6;
 %
-inclusions={{'sym','umi'},{'symtent','addtree'}};
+inclusions={{'sym','umi'},{'symtent','addtree'},{'addtree','addtree_check'}};
 %
 vtypes=fieldnames(corresp);
 for iv=1:length(vtypes)
@@ -41,6 +41,16 @@ for iv=1:length(vtypes)
             V_edge_counts.(vtype)(id+1),id,V_all.(vtype)(id+1)));
     end
 end
+%recalculate Vaddtree via method in manuscript
+Vc=ones(3,3,3,3,3,3);
+tau_list=[1 1;1 0;0 1]; %pairs of values for (tau_1,tau_4) and, independenty, for (tau_2,tau5),
+for t14=1:3
+    for t25=1:3
+        Vc(2+tau_list(t14,1),2-tau_list(t25,1),:,2-tau_list(t14,2),2+tau_list(t25,2),:)=0;
+    end
+end
+V.addtree_check=V.symtent.*Vc.*permute(Vc,[2 3 1 5 6 4]).*permute(Vc,[3 1 2 6 4 5]);
+V_binary.addtree_check=V.addtree_check([1 3],[1 3],[1 3],[1 3],[1 3],[1 3]);
 % check inclusion
 for iinc=1:length(inclusions)
     disp(sprintf('testing that V%-10s includes V%-10s',inclusions{iinc}{1},inclusions{iinc}{2}));
@@ -50,6 +60,17 @@ for iinc=1:length(inclusions)
         sum(Vden(:).*(Vden(:)-Vnum(:))),sum(Vnum(:).*(Vnum(:)-Vden(:)))));
     Vnum=V_binary.(inclusions{iinc}{1});
     Vden=V_binary.(inclusions{iinc}{2});
-    disp(sprintf('full:    %4.0f exceptions (reverse: %4.0f exceptions)',...
+    disp(sprintf('binary:  %4.0f exceptions (reverse: %4.0f exceptions)',...
         sum(Vden(:).*(Vden(:)-Vnum(:))),sum(Vnum(:).*(Vnum(:)-Vden(:)))));
 end
+%recalculate Vaddtree
+Vc=ones(3,3,3,3,3,3);
+tau_list=[3 3;3 2;2 3];
+for t14=1:3
+    for t25=1:3
+        Vc(tau_list(t14,1),4-tau_list(t25,1),:,4-tau_list(t14,2),tau_list(t25,2),:)=0;
+    end
+end
+V.addtree_check=V.symtent.*Vc.*permute(Vc,[2 3 1 5 6 4]).*permute(Vc,[3 1 2 6 4 5]);
+
+
