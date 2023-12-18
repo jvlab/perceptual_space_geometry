b915775e6e747a2ee3367ef15e9a4195c8909f42
Jonathan D Victor <jdvicto@med.cornell.edu>
2023-12-18 10:15:16 -0500

    Add files via upload
    psg_geo_pwaffine_test done

--- psg-code/psg_geo_pwaffine_test.m
+++ psg-code/psg_geo_pwaffine_test.m
@@ -42,6 +42,24 @@ transform_list=getinp('transforms to simulate','d',[1 ntransforms],transform_lis
 transform_fit=cell(ntransforms,nlayouts);
 coords_fit=cell(ntransforms,nlayouts);
 d_fit=zeros(ntransforms,nlayouts);
+d_init_min=zeros(ntransforms,nlayouts); %minimum d after brute force search
+%parameters to explore
+plists=struct;
+%
+plists.n_cuts_init.list={3,7,15,31};
+plists.n_cuts_init.fmt='%3.0f';
+%
+plists.if_orth.list={0,1};
+plists.if_orth.fmt='%3.0f';
+%
+plists.hsphere_method.list={'axes_and_orthants','random','fibspiral'};
+plists.hsphere_method.fmt='%18s';
+%
+plists.hsphere_nsamps.list={8,16,32,64,128 256 512};
+plists.hsphere_nsamps.fmt='%3.0f';
+plists.hsphere_nsamps.aux.hsphere_method={'random','fibspiral'};
+%
+if ~exist('n_timing') n_timing=2; end %number of repeats for timing
 for ilptr=1:length(layout_list)
     il=layout_list(ilptr);
     disp(' ');
@@ -66,6 +84,46 @@ for ilptr=1:length(layout_list)
         opts_model=model_types_def.(model_type).opts;
         [d_fit(it,il),coords_fit{it,il},transform_fit{it,il},opts_model_used]=psg_geo_general(coords_transformed,coords_orig,model_class,opts_model);
         disp(sprintf(' transforming with %30s, fitting with model type %30s: d=%8.5f',transforms{it}.label,model_type,d_fit(it,il)));
+        d_init_min(it,il)=opts_model_used.d_init_min;
+        %
+        disp('re-fitting with alternative optimization parameters');
+        disp(sprintf('                                           standard d_fit: %12.9f                     d_init: %12.9f',d_fit(it,il),d_init_min(it,il)));
+        pnames=fieldnames(plists);
+        for iname=1:length(pnames)
+            pname=pnames{iname};
+            plist=plists.(pname).list;
+            opts_model_try=opts_model; %base params
+            if isfield(plists.(pname),'aux') %outer loop
+                auxnames=fieldnames(plists.(pname).aux); %should only have one value
+                auxname=auxnames{1};
+                auxvals=plists.(pname).aux.(auxname);
+                naux=length(auxvals);
+            else
+                auxname=[];
+                naux=1;
+            end
+            for iaux=1:naux
+                if ~isempty(auxname)
+                    opts_model_try.(auxname)=auxvals{iaux};
+                    disp(auxvals{iaux});
+                end
+                for pptr=1:length(plist)
+                    opts_model_try.(pname)=plist{pptr};
+                    tic;
+                    for irept=1:n_timing
+                        [d_fit_try,coords_fit_try,transform_fit_try,opts_model_try_used]=psg_geo_general(coords_transformed,coords_orig,model_class,opts_model_try);
+                    end
+                    t_elapse=toc/n_timing;
+                    plabel=cat(2,sprintf('%20s=',pname),sprintf(plists.(pname).fmt,plist{pptr}));
+                    disp(sprintf('%40s: d_fit_try-d_fit: %12.9f, d_init_min_try-d_init_min: %12.9f, iters: %5.0f,  func calls: %5.0f, time: %8.5f sec',...
+                        plabel,...
+                        d_fit_try-d_fit(it,il),opts_model_try_used.d_init_min-d_init_min(it,il),...
+                        opts_model_try_used.fmin.output.iterations,...
+                        opts_model_try_used.fmin.output.funcCount,...
+                        t_elapse));
+                end %pptr
+            end %iaux
+        end % pnames
         %
     end %itptr
 end %il
