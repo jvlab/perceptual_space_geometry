205bf3c307cabeeaa86df5a0be451d0958414cd8
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-02-20 12:11:04 -0500

    Add files via upload
    psg_coord_pipe_proc now does basic plotting

--- psg-code/psg_coord_pipe_proc.m
+++ psg-code/psg_coord_pipe_proc.m
@@ -1,5 +1,6 @@
-%psg_pipe_coord_proc reads one or more coordinate datasets and processes, either by forming a consensus
-% or simple transformations
+%psg_pipe_coord_proc reads one or more coordinate datasets from experiments 
+% processes, either by forming a consensus or simple transformations,
+% and plots
 %
 % The output datafile has the fields
 %   dim[a], where [a] is a character string 1,...,7,8,9,10,
@@ -13,15 +14,20 @@
 %
 % 18Feb24: allow for propagtion of pipeline; show available files after each processing step
 %
-%  See also: PSG_GET_COORDSETS, PSG_QFORM2COORD_PROC, PSG_READ_COORDDATA, PSG_WRITE_COORDDATA.
+%  See also: PSG_GET_COORDSETS, PSG_QFORM2COORD_PROC, PSG_READ_COORDDATA, PSG_WRITE_COORDDATA, PSG_PLOTCOORDS.
 %
 if ~exist('opts_read') opts_read=struct();end %for psg_read_coord_data
 if ~exist('opts_rays') opts_rays=struct(); end %for psg_findrays
 if ~exist('opts_pcon') opts_pcon=struct(); end %for procrustes_consensus
 if ~exist('opts_write') opts_write=struct(); end %for psg_write_coorddata
+if ~exist('opts_plot') opts_plot=struct(); end % for psg_plotcoords
 %
-pipe_types_long={'done (and proceed to write data files)','create a consensus dataset from datasets with matching entries','Procrustes alignment to a given set'};
-pipe_types={'','consensus','Procrustes'};
+pipe_types_long={'done (and proceed to write data files)',...
+    'create a consensus dataset from datasets with matching entries',...
+    'PCA rotation of individual dataset(s)',...
+    'Procrustes alignment to a given set',...
+    'plot coordinates of individual dataset(s) [no new datasets created]'};
+pipe_types={'','consensus','PCA_rotation','Procrustes','plot_coords'};
 npipe_types=length(pipe_types)-1;
 %
 disp('This will process one or more coordinate datasets and create new coordinate datasets, via simple transformations or consensus.');
@@ -67,41 +73,42 @@ if (nsets>1)
         end
         pipe_type=getinp('choice','d',[0 npipe_types]);
         if pipe_type>0
+            proc_sets=getinp('datasets to process','d',[1 nsets],[1:nsets_orig]);
+            proc_sets_string=sprintf('%1.0f ',proc_sets);
+            nproc_sets=length(proc_sets);
             switch pipe_types{pipe_type+1}
                 case 'consensus'
-                    con_sets=getinp('datasets to combine','d',[1 nsets],[1:nsets_orig]);
-                    con_sets_string=sprintf('%1.0f ',con_sets);
-                    if min(nstims_each(con_sets))~=max(nstims_each(con_sets)) %should have been blocked by psg_read_coordsets
+                     if min(nstims_each(proc_sets))~=max(nstims_each(proc_sets)) %should have been blocked by psg_read_coordsets
                         disp('number of stimuli do not match.');
                     else
-                        nstims=min(nstims_each(con_sets));
+                        nstims=min(nstims_each(proc_sets));
                         allow_scale=getinp('1 to allow scaling','d',[0 1]);
                         z=cell(1,dim_max);
-                        znew=cell(1,dim_max); %datasets after transformatoin
+                        znew=cell(1,dim_max); %datasets after transformation
                         consensus=cell(1,dim_max); %consensus
                         ts=cell(1,dim_max); %transformations
                         file_list=cell(0);
                         sets_combined=cell(0);
                         for id=1:dim_max
-                            z{id}=zeros(nstims,id,length(con_sets));
-                            for iset_ptr=1:length(con_sets)
-                                iset=con_sets(iset_ptr);
+                            z{id}=zeros(nstims,id,nproc_sets);
+                            for iset_ptr=1:nproc_sets
+                                iset=proc_sets(iset_ptr);
                                 z{id}(:,:,iset_ptr)=ds{iset}{id};
                                 file_list{iset_ptr}=labels{iset};
                                 sets_combined{iset_ptr}=sets{iset};
                             end
-                            [consensus{id},znew{id},ts{id},details,opts_pcon_used]=procrustes_consensus(z{id},setfield(opts_pcon,'allow_scale',allow_scale));
+                            [consensus{id},znew{id},ots{id},details,opts_pcon_used]=procrustes_consensus(z{id},setfield(opts_pcon,'allow_scale',allow_scale));
                             disp(sprintf('calculated consensus for %3.0f dimensions; iterations: %4.0f',id,length(details.ts_cum)));
                         end
                         %create data and metadata for each old dataset rotated into consensus
-                        for iset_ptr=1:length(con_sets)
+                        for iset_ptr=1:nproc_sets
                             nsets=nsets+1; %a new dataset for each file rotated into consensus
                             for id=1:dim_max
                                 ds{nsets}{id}=znew{id}(:,:,iset_ptr);
                             end
-                            iset=con_sets(iset_ptr);
+                            iset=proc_sets(iset_ptr);
                             sas{nsets}=sas{iset}; %take previous stimulus descriptors
-                            labels{nsets}=sprintf('set %1.0f rotated into consensus of sets %s, allow_scale=%1.0f',iset,con_sets_string,allow_scale);
+                            labels{nsets}=sprintf('set %1.0f rotated into consensus of sets %s, allow_scale=%1.0f',iset,proc_sets_string,allow_scale);
                             nstims_each(nsets)=nstims;
                             nextpipe=length(pipelines{iset})+1;
                             pipelines{nsets}{nextpipe}.type='rotate_to_consensus';
@@ -114,8 +121,8 @@ if (nsets>1)
                         nsets=nsets+1; %a new dataset for each file rotated into consensus
                         nstims_each(nsets)=nstims;
                         ds{nsets}=consensus;
-                        sas{nsets}=sas{con_sets(1)} %assume all stimulus descriptors are the same
-                        labels{nsets}=sprintf('consensus of sets %s, allow_scale=%1.0f',con_sets_string,allow_scale);
+                        sas{nsets}=sas{proc_sets(1)} %assume all stimulus descriptors are the same
+                        labels{nsets}=sprintf('consensus of sets %s, allow_scale=%1.0f',proc_sets_string,allow_scale);
                         nstims_each(nsets)=nstims;
                         nextpipe=1; %de novo dataset
                         pipelines{nsets}{nextpipe}.type='consensus';
@@ -124,6 +131,64 @@ if (nsets>1)
                         pipelines{nsets}{nextpipe}.sets_combined=sets_combined;
                     end
                 case 'Procrustes'
+                    disp('not implemented yet');
+                case 'PCA_rotation'
+                    ifok=0;
+                    while (ifok==0)
+                        sel=getinp('selection string or multiple strings, separated by |','s',[]);
+                        typenames_sel=cell(nproc_sets,1);
+                        typenames_inds=cell(nproc_sets,1);
+                        for iset_ptr=1:nproc_sets
+                            iset=proc_sets(iset_ptr);                   
+                            [typenames_sel{iset_ptr},typenames_inds{iset_ptr}]=psg_select_util(sel,sas{iset});
+                            disp(sprintf('dataset %2.0f (%30s): %2.0f stimuli, %2.0f stimuli selected',iset,labels{iset},nstims_each(iset),length(typenames_sel{iset_ptr})));
+                            disp('typenames selected:');
+                                disp(typenames_sel{iset_ptr});
+                        end
+                        ifok=getinp('1 if ok','d',[0 1],0);
+                        if_center=getinp('1 to center the data','d',[0 1],1);
+                        for iset_ptr=1:nproc_sets
+                            iset=proc_sets(iset_ptr); 
+                            nsets=nsets+1; %a new dataset for each file rotated into consensus
+                            for id=1:dim_max
+                                d_unrot=ds{iset}{id};
+                                %ds{nsets}{id}=znew{id}(:,:,iset_ptr);
+                            end
+                        end
+                    end %next iset_ptr to rotate
+                    disp('not implemented yet')
+                case 'plot_coords'
+                    dim_plot=getinp('dimension to plot','d',[2 dim_max]);
+                    dim_select=getinp('dimension selection','d',[1 dim_plot],[1:min(dim_plot,4)]);
+                    opts_plot_used=cell(1,nsets);
+                    for iset_ptr=1:nproc_sets
+                        iset=proc_sets(iset_ptr);
+                        tstring_name=sprintf('dataset %2.0f (%30s)',iset,labels{iset});
+                        tstring_dim=cat(2,sprintf('dim %1.0f',dim_plot),' [',sprintf(' %1.0f',dim_select),' ]');
+                        %
+                        figure;
+                        set(gcf,'Position',[50 50 1200 800]);
+                        set(gcf,'NumberTitle','off');
+                        set(gcf,'Name',cat(2,tstring_name,' ',tstring_dim));
+                        opts_plot_use=opts_plot;
+                        opts_plot_use.axis_handle=gca;
+                        if (iset<=nsets_orig)
+                            rays_use=rayss{iset};
+                        else %create a ray structure if possible
+                            if isfield(sas{iset},'btc_specoords') %check if btc_specoords is present and if not use a noray option
+                                rays_use=psg_findrays(sas{iset}.btc_specoords,opts_rays);
+                            else
+                                rays_use=[];
+                            end
+                        end
+                        if isempty(rays_use)
+                            opts_plot_use.if_use_rays=0;
+                        end
+                        opts_plot_used{iset}=psg_plotcoords(ds{iset}{dim_plot},dim_select,sas{iset},rays_use,opts_plot_use);
+                        axes('Position',[0.01,0.02,0.01,0.01]); %for text
+                        text(0,0,cat(2,tstring_name,' ',tstring_dim),'Interpreter','none','FontSize',8);
+                        axis off;
+                    end %next iset_ptr to plot
             end
         else
             if_done=1;
