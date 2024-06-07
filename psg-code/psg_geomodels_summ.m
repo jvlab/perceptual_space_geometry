38ecd1ee02d2360ef480bc8c442791004e5186c5
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-06-07 15:56:54 -0400

    Add files via upload
    add omnibus plot

--- psg-code/psg_geomodels_summ.m
+++ psg-code/psg_geomodels_summ.m
@@ -1,5 +1,10 @@
 %psg_geomodels_summ: summarize analysis of geometric models
-% and compare models with nested models
+% omnibus plot, and comparisoin of models with nested models
+%
+% The plots are 3d, and assume that both adj model and reference model are
+% examined for more than one dimension.
+%   to do: make it work for one-dimension analyses, with 
+%   hs=surf([4 4],[1:5],repmat(rand(5,1),1,2));set(hs,'FaceColor','none') or similar
 %
 %runs on "results" variable created by psg_geomodels_run
 %
@@ -12,6 +17,7 @@
 if ~exist('sig_level') sig_level=0.05; end
 if ~exist('sig_symbols') sig_symbols={'+','x'}; end
 if ~exist('colors_mn') colors_mn={'k','b'}; end
+if ~exist('colors_models') colors_models={'k','b','c','m','r',[1 0.5 0],[0.7 0.7 0],'g'}; end
 %
 fn=getinp('mat-file from psg_geomodels_run with results to summarize','s',[]);
 if_log=getinp('1 for detailed log','d',[0 1],0);
@@ -80,7 +86,55 @@ for icompare=1:size(compares,1)
         disp(sprintf('for model %s and nested model %s: critical=%1.0f',model_type,nested_type,crit))
     end
 end %icompare
+%
+%collect values of d across all models
+%
+d_models=zeros(length(ref_dim_list),length(adj_dim_list),length(model_types));
+for imodel=1:length(model_types)
+    model_type=model_types{imodel};
+    %
+    %collect values of d across all dimensions and models
+    %
+    for ref_dim_ptr=1:length(ref_dim_list)
+        ref_dim=ref_dim_list(ref_dim_ptr);
+        for adj_dim_ptr=1:length(adj_dim_list)
+            adj_dim=adj_dim_list(adj_dim_ptr);
+            d_models(ref_dim_ptr,adj_dim_ptr,imodel)=results{ref_dim,adj_dim}.d(imodel);
+        end
+    end
+end
+%
+%make omnibus plot
+%
+figure;
+tstring_omni='all models';
+set(gcf,'Position',[100 100 1200 800]);
+set(gcf,'NumberTitle','off');
+set(gcf,'Name',tstring_omni);
+hold on;
+for imodel=1:length(model_types)
+    h_model=surf(adj_dim_list,ref_dim_list,d_models(:,:,imodel));
+    set(h_model,'FaceColor','none');
+    set(h_model,'EdgeColor',colors_models{1+mod(imodel-1,length(colors_models))});
+end
+xlabel('adj dim');
+ylabel('ref dim');
+set(gca,'XTick',adj_dim_list);
+set(gca,'YTick',ref_dim_list);
+zlabel('d');
+set(gca,'ZLim',[0 1]);
+grid on;
+box on;
+view(3);
+hl=legend(strvcat(model_types));
+set(hl,'Interpreter','none');
+%
+axes('Position',[0.01,0.02,0.01,0.01]); %for text
+text(0,0,cat(2,tstring_omni,' ',fn),'Interpreter','none');
+axis off;
+%
 %do comparisons
+%
 for icompare=1:size(compares,1)
     imodel=compares(icompare,1);
     inest=compares(icompare,2);
@@ -89,7 +143,7 @@ for icompare=1:size(compares,1)
     if compares(icompare,3)==1 | if_allcompare==1
         disp(sprintf('comparing model %s and nested model %s',model_type,nested_type))
         %
-        d_model=zeros(length(ref_dim_list),length(adj_dim_list));
+        d_model=d_models(:,:,imodel);
         d_nest=zeros(length(ref_dim_list),length(adj_dim_list));
         if_sig=zeros(length(ref_dim_list),length(adj_dim_list),2); %d3 is normalization type      
         %
@@ -98,7 +152,7 @@ for icompare=1:size(compares,1)
             ref_dim=ref_dim_list(ref_dim_ptr);
             for adj_dim_ptr=1:length(adj_dim_list)
                 adj_dim=adj_dim_list(adj_dim_ptr);
-                d_model(ref_dim_ptr,adj_dim_ptr)=results{ref_dim,adj_dim}.d(imodel);
+%                d_model(ref_dim_ptr,adj_dim_ptr)=results{ref_dim,adj_dim}.d(imodel);
                 d_nest(ref_dim_ptr,adj_dim_ptr)=results{ref_dim,adj_dim}.d(inest);
                 for norm_type=1:2
                     if_sig(ref_dim_ptr,adj_dim_ptr,norm_type)=double(results{ref_dim,adj_dim}.surrogate_count(imodel,inest,norm_type)<sig_level*nshuff);
