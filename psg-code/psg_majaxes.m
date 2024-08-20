7c929c5ca1c1355e49ceee10970eb5c200235afd
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-08-20 18:10:33 -0400

    Add files via upload
    additional flexibility in flipping signs of eigenvectors; also displays original coords

--- psg-code/psg_majaxes.m
+++ psg-code/psg_majaxes.m
@@ -19,10 +19,13 @@ function [results,opts_used]=psg_majaxes(d_ref,sa_ref,d_adj,sa_adj,results_geo,o
 %     opts.plot_pairs: size [nplots 2], each row is [ref dim, adj dim] to plot, defaults to zeros(0,2)
 %     opts.plot_colormap: color map for plots, defaults to 'jet'
 %     opts.plot_submean: 1 to subtract means from plots
-%     opts.plot_flipsign: 1 to allow sign of projections to be flipped to best match across piecewise transforms
+%     opts.plot_flipsign:
+%           'within_set' to flip sign of projections to be best match across piecewise transforms, with adj and ref
+%           'across_set' to flip signs of projections to match with adj dataset, across both adj and ref
 %     opts.plot_order: cell array in order to plot, each should match an element of sa_ref.typenames orsa_adj.typenames.
 %         If omitted, all are plotted in the same order as in typenames
 %         Overridden by opts.plot_order_[ref|adj] for reference and adjusted datasets
+%     opts.plot_coords: 1 to also plot original coordinates
 %
 %  results: analysis results
 %  opts_used: options used
@@ -30,6 +33,8 @@ function [results,opts_used]=psg_majaxes(d_ref,sa_ref,d_adj,sa_adj,results_geo,o
 %      figh is figh(iplot,im_ptr): one plot for each geometric model analyzed)
 %
 %  17Aug24: can specify order of plots; fix bug in display of eigenvalues
+%  19Aug24: add plot_coords to plot original coordinates; fix vertical size of eigenvector display;
+%           add flexibility in opts.plot_flipsign
 %
 %   See also: PSG_GEOMODELS_RUN, PSG_GEOMODELS_DEFINE.
 %
@@ -42,10 +47,11 @@ opts=filldefault(opts,'tol_match',10^-4);
 opts=filldefault(opts,'plot_pairs',zeros(0,2));
 opts=filldefault(opts,'plot_colormap','jet');
 opts=filldefault(opts,'plot_submean',1);
-opts=filldefault(opts,'plot_flipsign',1);
+opts=filldefault(opts,'plot_flipsign','within_set');
 opts=filldefault(opts,'plot_order',[]);
 opts=filldefault(opts,'plot_order_ref',opts.plot_order);
 opts=filldefault(opts,'plot_order_adj',opts.plot_order);
+opts=filldefault(opts,'plot_coords',1);
 %
 model_types_def=psg_geomodels_define();
 opts.model_types_def=model_types_def;
@@ -180,7 +186,7 @@ for iplot=1:nplots
             set(gcf,'NumberTitle','off');
             set(gcf,'Name',fig_label);
             npw=size(res_plot.ref.eivecs{im_ptr},3);
-            nrows=max(2,npw);
+            nrows=max(2,npw+opts.plot_coords);
             if (opts.if_log)
                 disp(sprintf('plotting %s',fig_label));
             end
@@ -189,18 +195,24 @@ for iplot=1:nplots
                     case 1
                         typenames_std=sa_adj.typenames;
                         typenames_plot=opts.plot_order_adj;
+                        coords=d_adj{adj_plot};
                     case 2
                         typenames_std=sa_ref.typenames;
                         typenames_plot=opts.plot_order_ref;
+                        coords=d_ref{ref_plot};
                 end
                 if isempty(typenames_plot)
                     typenames_plot=typenames_std;
                 end
-                for ipw=1:npw
+                for ipw=1-opts.plot_coords:npw %0 to plot pcs
                     lab=adj_ref_labels{iar};
-                    subplot(nrows,nar+1,(nar+1)*(ipw-1)+iar);
+                    subplot(nrows,nar+1,(nar+1)*(ipw-1+opts.plot_coords)+iar);
                     %do calculations for all stimuli, then select
-                    z=res_plot.(lab).projections{im_ptr}(:,:,ipw); %projections
+                    if (ipw==0) %plot coordinates
+                        z=coords;
+                    else
+                        z=res_plot.(lab).projections{im_ptr}(:,:,ipw); %projections
+                    end
                     nstims=size(z,1);
                     neivs=size(z,2);
                     if opts.plot_submean
@@ -208,12 +220,28 @@ for iplot=1:nplots
                     end
                     flips=cell(1,neivs);
                     meiv_labels=cell(1,neivs);
-                    if opts.plot_flipsign
-                        if (ipw==1)
-                            z_align=z;
-                        else
-                            %if more than one transform, flip signs for the later transforms so that there
-                            %is the closest match to the first transform
+                    if_align=0;
+                    if ipw>0 %do not attempt to align coords, just eigenvectors
+                        switch opts.plot_flipsign
+                            case 'within_set'
+                                if ipw==1
+                                    z_align=z;
+                                else
+                                    if_align=1;
+                                end
+                            case 'across_set'
+                                if ipw==1 & iar==1
+                                    z_align=z;
+                                else
+                                    if_align=1;
+                                end
+                            otherwise
+                                if_align=0;
+                        end
+                        if if_align
+                            %flip signs for the eigenvectors 
+                            %to find closest match to eigenvectors of first
+                            %transform, either within adj|ref or adj
                             zdots=z'*z_align;
                             meiv=zeros(1,neivs);
                             for ieiv=1:neivs
@@ -228,7 +256,7 @@ for iplot=1:nplots
                                 meiv_labels{ieiv}=sprintf('[%1se%1.0f]',flips{ieiv},meiv(ieiv)); %convert to char
                             end
                         end
-                    end
+                    end %ipw~=0
                     %now select and reorder
                     [zplot,xtick_labels]=psg_majaxes_reorder(z,typenames_plot,typenames_std);
                     nstims_plot=size(zplot,1);
@@ -237,13 +265,25 @@ for iplot=1:nplots
                     set(gca,'XTick',[1:nstims_plot]);
                     set(gca,'XTickLabel',xtick_labels);
                     set(gca,'YTick',[1:neivs]); %label each eigenvector with magnif factor
+                    set(gca,'YLim',0.5+[0 max(ref_plot,adj_plot)]); %extend vertical axis so that they match
+                    set(gca,'Color',get(gcf,'Color')); %fix background
                     ytl=cell(1,neivs);
-                    for ieiv=1:neivs
-                        ytl{ieiv}=sprintf('%s eiv %1.0f: x %5.2f',meiv_labels{ieiv},ieiv,res_plot.(lab).magnifs{im_ptr}(ieiv,ipw));
+                    if ipw>0
+                        for ieiv=1:neivs
+                            ytl{ieiv}=sprintf('%s eiv %1.0f: x %5.2f',meiv_labels{ieiv},ieiv,res_plot.(lab).magnifs{im_ptr}(ieiv,ipw));
+                        end
+                    else
+                        for ieiv=1:neivs
+                            ytl{ieiv}=sprintf('coord %1.0f',ieiv);
+                        end
                     end
                     set(gca,'YTickLabel',ytl);
                     colormap(opts.plot_colormap);
-                    title(sprintf('%s, transform %1.0f',lab,ipw));
+                    if ipw>0
+                        title(sprintf('%s, transform %1.0f',lab,ipw));
+                    else
+                        title(sprintf('%s, coords',lab));
+                    end
                 end %ipw
             end %adj or ref
             subplot(1,nar+1,nar+1);
