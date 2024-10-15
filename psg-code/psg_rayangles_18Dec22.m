8ddd57f4c87cc83952d4a0dd1028666a2f661527
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-10-15 11:22:36 -0400

    Add files via upload
    documentation and labelling improvements in psg_rayangles; initial versoin of psg_raymults

--- psg-code/psg_rayangles_18Dec22.m
+++ psg-code/psg_rayangles_18Dec22.m
@@ -2,10 +2,6 @@ function [angles,opts_used]=psg_rayangles(ray_ends,sa,rays,opts)
 % [angles,opts_used]=psg_rayangles(ray_ends,sa,rays,opts) computes angles between rays
 % from a set of coordinates in a psg experiment
 %
-%  two modes, determined by np=size(ray_ends,2) (if_bid=2-np))
-%    np=2 (if_bid=0): each polarity considered separately [neg, pos]
-%    np=1 (if_bid=1): bidirectional calculation, neg and positive rays considered as part of a line segment
-%
 % ray_ends: regression coefs at ends of each ray [nrays nd 2-if_bid], typically from psg_rayfit
 %    dim 3 is neg then pos for unidirectional rays (if_bid=0)
 %    dim 3 is length 1 for bidirectional rays (if_bid=1)
@@ -20,10 +16,7 @@ function [angles,opts_used]=psg_rayangles(ray_ends,sa,rays,opts)
 %   ang_degs: anlges in degress (iray,jray, isign, jsign)
 % opts_used: options used
 %
-% 15Oct24: immprove documentation, labels created even if no logging; improve label justification
-%
-%   See also:  PSG_READCOORD_DATA, PSG_RAYFIT, FILLDEFAULT, PSG_VISUALIZE_DEMO, PSG_FINDRAYS, PSG_PLOTANGLES
-%   PSG_RAYMULTS.
+%   See also:  PSG_READCOORD_DATA, PSG_RAYFIT, FILLDEFAULT, PSG_VISUALIZE_DEMO, PSG_FINDRAYS, PSG_PLOTANGLES.
 %
 if (nargin<4)
     opts=struct;
@@ -79,31 +72,29 @@ angles.ang_degs=acos(max(min(cosangs,1),-1))*(180/pi);
 %
 if (opts.if_log)
     disp(sprintf('angles (deg) between rays in a model with %3.0f dimensions',nd));
-end
-for iray=1:nrays
-    for jray=1:nrays
-        str=[];
-        for ip=1:np
-            for jp=1:np
-                ipoints=intersect(find(rays.whichray==iray),sign_select{ip});
-                ilab=sa.typenames{ipoints(end)};
-                jpoints=intersect(find(rays.whichray==jray),sign_select{jp});
-                jlab=sa.typenames{jpoints(end)};
-                %
-                ijp=(ip-1)*np+jp;
-                labels{iray,jray,ijp}=cat(2,sprintf('%12s',ilab),' ',sprintf('%12s',jlab)); %15Oct24
-                str=cat(2,str,labels{iray,jray,ijp},sprintf(': %7.3f  ',angles.ang_degs(iray,jray,ijp)));
-            end %jp
-        end %ip
-        if opts.if_log
+    for iray=1:nrays
+        for jray=1:nrays
+            str=[];
+            for ip=1:np
+                for jp=1:np
+                    ipoints=intersect(find(rays.whichray==iray),sign_select{ip});
+                    ilab=sa.typenames{ipoints(end)};
+                    jpoints=intersect(find(rays.whichray==jray),sign_select{jp});
+                    jlab=sa.typenames{jpoints(end)};
+                    %
+                    ijp=(ip-1)*np+jp;
+                    labels{iray,jray,ijp}=cat(2,ilab,' ',jlab);
+                    str=cat(2,str,labels{iray,jray,ijp},sprintf(': %7.3f  ',angles.ang_degs(iray,jray,ijp)));
+                end %jp
+            end %ip
             if (jray<iray) | ((jray==iray) & np==2) %display only unique data
                 disp(str);
             end
             if (jray==iray)
                 disp(' ');
             end
-        end
+        end %iray
     end %jray
-end %iray
-opts_used.labels=labels;
+    opts_used.labels=labels;
+end
 return
---
+++ psg-code/psg_raymults.m
@@ -0,0 +1,78 @@
+function [mults,opts_used]=psg_raymults(ray_ends,sa,rays,opts)
+% [mults,opts_used]=psg_raymults(ray_ends,sa,rays,opts) computes multipliers for
+% lengths of rays from a set of coordinates in a btc experiment
+%
+%  the "multiplier" is the length, in coordinate units, of the linear extrapolation of a 
+%  best-fitting ray to a correlation strength of 1
+%
+%  two modes, determined by np=size(ray_ends,2) (if_bid=2-np))
+%    np=2 (if_bid=0): each polarity considered separately [neg, pos]
+%    np=1 (if_bid=1): bidirectional calculation, neg and positive rays considered as part of a line segment
+%     and as part of a single regression
+%
+% ray_ends: regression coefs at ends of each ray [nrays nd 2-if_bid], typically from psg_rayfit
+%    dim 3 is neg then pos for unidirectional rays (if_bid=0)
+%    dim 3 is length 1 for bidirectional rays (if_bid=1)
+% sa: the setup structure returned from psg_readcoord_data (needed for labeling if opts.if_log=1)
+% rays: a ray structure, typically returned by psg_findrays
+% opts: options
+%  opts.if_log=1: to log results
+%
+% mults: each field is dim 1 is ray, dim 2 is [neg, pos] if np=2, trivial if np=1,
+%   dist_gain: distance  for a unit change in correlation strength
+%   dist_raw:  %raw distances to largest correlation strength available
+%   dist_endpts: %distances to endpoints (dist_mult=dist_raw./repmat(dist_endpts,1,np)
+% opts_used: options used
+%
+%   See also:  PSG_READCOORD_DATA, PSG_RAYFIT, FILLDEFAULT, PSG_VISUALIZE_DEMO, PSG_FINDRAYS, PSG_RAYANGLES.
+%
+if (nargin<4)
+    opts=struct;
+end
+opts=filldefault(opts,'if_log',0);
+%
+nrays=size(ray_ends,1);
+nd=size(ray_ends,2);
+%if_bid=2-size(ray_ends,3);
+np=size(ray_ends,3); %number of polarities
+%
+opts_used=opts;
+dotprods=zeros(nrays,nrays,np,np);
+dists=zeros(nrays,np); %raw distances to endpoints (np=2) or distances between endpoints (np=1)
+labels=cell(nrays,np);
+for iray=1:nrays
+    for ip=1:np
+        dists(iray,ip)=sqrt(sum(ray_ends(iray,:,ip).^2)); 
+    end %ip
+end %iray
+mults.dist_raw=dists;
+%normalize by endpoints
+dist_endpts=sqrt(sum(rays.endpt.^2,2));
+mults.dist_endpts=dist_endpts;
+mults.dist_gain=dists./repmat(dist_endpts,1,np);
+sign_select=cell(np,1);
+if np==1
+    sign_select{1}=find(rays.mult~=0);
+else
+    sign_select{1}=find(rays.mult<0);
+    sign_select{2}=find(rays.mult>0);
+end
+%
+if (opts.if_log)
+    disp(sprintf('distances along rays in a model with %3.0f dimensions',nd));
+end
+for iray=1:nrays %section retains logic of psg_rayangs
+    str=[];
+    for ip=1:np
+        ipoints=intersect(find(rays.whichray==iray),sign_select{ip});
+        ilab=sa.typenames{ipoints(end)};
+        %
+        labels{iray,ip}=sprintf('%12s',ilab);
+        str=cat(2,str,labels{iray,ip},sprintf(': %7.3f  (max coord dist: %7.3f)  ',mults.dist_gain(iray,ip),mults.dist_endpts(iray)));
+    end %ip
+    if (opts.if_log)
+        disp(str);
+    end
+end %iray
+opts_used.labels=labels;
+return
