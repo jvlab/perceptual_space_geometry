5d1692cc069a8300e50b0726e79b6bd80d060f5d
Jonathan D Victor <jdvicto@med.cornell.edu>
2024-12-25 21:23:37 -0500

    Add files via upload
    allow for multiple redundant rows in specifying translation; documentation enhancements

--- psg-code/psg_geomodels_apply.m
+++ psg-code/psg_geomodels_apply.m
@@ -1,5 +1,5 @@
 function coords_new=psg_geomodels_apply(model_class,coords,transform)
-% coords_new=psg_geomodels_apply(model_class,coords,transform) applies any of severalsurf
+% coords_new=psg_geomodels_apply(model_class,coords,transform) applies any of several
 % geometric models to a coordinate set
 %
 % model_class: one of the model classes in psg_geomodels_define, typically one of 
@@ -7,13 +7,20 @@ function coords_new=psg_geomodels_apply(model_class,coords,transform)
 % coords: coordinates: size is [npts,ndims]
 % transform: a structure containing the transform parameters, typically
 %    transform.b: scale
-%    transform.T: [ndims,ndims]: a post-multiplying matrix
-%    transorms.c: offset, [1 ndims]
-%    also may include a cutpoint, or a projective parameter
+%    transform.T: [ndims,ndims]: a post-multiplying matrix or, for 
+%      pwaffine and pwprojective, a stack of 2^ncut matrices
+%    transform.c: offset, [1 ndims] 
+%      Note this differs from matlab's procrustes output
+%      * For mean, procrustes, affine, or projective, only the first row is needed;
+%      other rows are redundant and ignored.
+%      * for pwaffine and pwprojective, the multiple rows have different meanings
+%    transform also may include specifications of cutplanes (vcut), cutpoints (acut)
+%      and projective parameters p (see psg_pwaffine_apply, psg_pwprojective_apply)
 %
 % coords_new: coordinates after transformation, size is [npts,ndims]
 %
-% 10July24: added pwprojective
+% 10Jul24: added pwprojective
+% 25Dec24: allow transform.c to have multiple rows for mean, procrutes, affine, projective
 %
 %   See also:  PSG_GEOMODELS_RUN, PROCRUSTES, PSG_GEO_GENERAL, PSG_GEOMODELS_DEFINE,
 %     PERSP_APPLY, PSG_PWAFFINE_APPLY, PSG_PWPROJECTIVE_APPLY.
@@ -21,10 +28,10 @@ function coords_new=psg_geomodels_apply(model_class,coords,transform)
 switch model_class
     case {'mean','procrustes','affine'}
        npts=size(coords,1);
-       coords_new=transform.b*coords*transform.T+repmat(transform.c,npts,1);
+       coords_new=transform.b*coords*transform.T+repmat(transform.c(1,:),npts,1);
     case 'projective'
         %note change of variable names between psg_geo_projective and persp_apply
-        coords_new=persp_apply(transform.T,transform.c,transform.p,coords);
+        coords_new=persp_apply(transform.T,transform.c(1,:),transform.p,coords);
     case 'pwaffine'
         coords_new=psg_pwaffine_apply(transform,coords);
     case 'pwprojective'
