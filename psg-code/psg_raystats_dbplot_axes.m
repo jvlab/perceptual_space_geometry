1f93adf070eb0f5b02991a444c38c70604d0e6c7
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-06-12 11:44:20 -0400

    Add files via upload
    more axis optoins

--- psg-code/psg_raystats_dbplot_axes.m
+++ psg-code/psg_raystats_dbplot_axes.m
@@ -8,10 +8,12 @@
 %
 axis_opt=-1;
 axis_opts={'revert to original',...
-    'set all axes to extreme of values in use'};
+    'set all axes to extreme of values in use',...
+    'set all axes to specified values'};
 subh_dims=[size(ylims,1),size(ylims,2),size(ylims,3)];
 ylim_global(1)=min(reshape(ylims(:,:,:,1),prod(subh_dims),1),[],1,'omitnan');
 ylim_global(2)=max(reshape(ylims(:,:,:,2),prod(subh_dims),1),[],1,'omitnan');
+ylim_spec=ylim_global;
 while axis_opt~=0
     disp('options for ordinate');
     for iadj=1:length(axis_opts)
@@ -19,6 +21,9 @@ while axis_opt~=0
     end
     disp(' 0->done');
     axis_opt=getinp('choice','d',[0 length(axis_opts)]);
+    if axis_opt==3
+        ylim_spec=getinp('axis range','f',[-Inf Inf],ylim_spec);
+    end
     %
     for irow=1:subh_dims(1)
         for icol=1:subh_dims(2)
@@ -30,6 +35,8 @@ while axis_opt~=0
                             set(subhandle,'YLim',ylims(irow,icol,ipage,:));
                         case 2
                             set(subhandle,'YLim',ylim_global);
+                        case 3
+                            set(subhandle,'YLim',ylim_spec);
                     end %axis_opt
                 end %isempty
             end %ipage
