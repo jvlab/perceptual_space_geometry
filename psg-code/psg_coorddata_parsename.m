9998d793ebcd35d25b69a59fe2224e2715eb3d00
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-10-03 10:02:39 -0400

    Add files via upload
    updated body of code, not just doc

--- psg-code/psg_coorddata_parsename.m
+++ psg-code/psg_coorddata_parsename.m
@@ -30,7 +30,7 @@ parsed.setup_fullname_def=opts.setup_fullname_def;
 parsed.domain_match=0;
 parsed.warn_string=[];
 %
-underscore_sep=min(strfind(data_fullname,opts.coord_string));
+underscore_sep=min(psg_strfind(data_fullname,opts.coord_string));
 if ~isempty(underscore_sep)
     for id=1:length(opts.domain_list)
         if contains(data_fullname,cat(2,opts.domain_list{id},'_coords'))
@@ -38,20 +38,20 @@ if ~isempty(underscore_sep)
         end
     end
     %
-    seps=max(union(strfind(data_fullname,'/'),strfind(data_fullname,'\')));
+    seps=max(union(psg_strfind(data_fullname,'/'),psg_strfind(data_fullname,'\')));
     if ~isempty(seps)
         parsed.data_shortname=parsed.data_shortname(seps+1:end);
     end
-    if ismember(1,strfind(parsed.data_shortname,'faces'))
+    if ismember(1,psg_strfind(parsed.data_shortname,'faces'))
         parsed.setup_fullname_def=cat(2,data_fullname(1:underscore_sep-1),'.mat');
         parsed.type_class='faces_mpi';
-    elseif ismember(1,strfind(parsed.data_shortname,'irgb'))
+    elseif ismember(1,psg_strfind(parsed.data_shortname,'irgb'))
         parsed.setup_fullname_def=cat(2,data_fullname(1:underscore_sep-1),'.mat');
         parsed.type_class='irgb';
-    elseif ismember(1,strfind(parsed.data_shortname,'mater'))
+    elseif ismember(1,psg_strfind(parsed.data_shortname,'mater'))
         parsed.setup_fullname_def=cat(2,data_fullname(1:underscore_sep-1),'.mat');
         parsed.type_class='mater';
-    elseif ismember(1,strfind(parsed.data_shortname,opts.type_class_aux))
+    elseif ismember(1,psg_strfind(parsed.data_shortname,opts.type_class_aux))
         parsed.setup_fullname_def=data_fullname;
         parsed.type_class=opts.type_class_aux;
         parsed.need_setup_file=0; %no setup file needed
