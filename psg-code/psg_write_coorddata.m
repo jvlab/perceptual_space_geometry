dc06f83a9e2f9b41a42371638767115560437984
Jonathan D Victor <jdvicto@med.cornell.edu>
2025-10-25 02:00:52 +0500

    Add files via upload
    additions for rs compatibility

--- psg-code/psg_write_coorddata.m
+++ psg-code/psg_write_coorddata.m
@@ -1,5 +1,5 @@
-function opts_used=psg_write_coorddata(data_fullname,ds,sout,opts)
-% opts_used=psg_write_coorddata(data_fullname,ds,sout,opts) writes a coordinate structure
+function [opts_used,s_written]=psg_write_coorddata(data_fullname,ds,sout,opts)
+% [opts_used,s_written]=psg_write_coorddata(data_fullname,ds,sout,opts) writes a coordinate structure
 %
 % data_fullname: data file name including path; if empty, requested interactively
 % ds: cell array, ds{k} is the scaling solution for the kth dimension
@@ -14,10 +14,13 @@ function opts_used=psg_write_coorddata(data_fullname,ds,sout,opts)
 %   opts.if_log: 1 to log
 %
 % opts_used: options used
+%   opts_used.warnings: list of warnings
+% s_written: structure written
 %
 % 28Nov23: multiple ways to determine number of stimuli
 % 22Feb24: localization params now from psg_localopts
 % 14Oct24: handle a coordinate set with no data
+% 25Oct25: added s_written, opts_used.warnings
 %
 % See also:  PSG_READ_COORDDATA, PSG_QFORM2COORD_PROC, PSG_LOCALOPTS, PSG_CONSENSUS_WRITE_UTIL,
 %    PSG_ALIGN_KNIT_DEMO, PSG_COORD_PIPE_PROC.
@@ -34,6 +37,8 @@ if isempty(data_fullname)
 end
 opts.data_fullname=data_fullname;
 opts_used=opts;
+opts_used.warnings=[];
+s_written=struct;
 %
 if isfield(sout,'stim_labels')
     nstims=size(sout.stim_labels,1);
@@ -49,16 +54,21 @@ for idimptr=1:length(ds)
     if (idim>0) %added 14Oct24
         dim_string=cat(2,dim_string,sprintf('%1.0f ',idim));
         if size(ds{idimptr},1)~=nstims
-            warning(sprintf('for dim %2.0f (pointer=%2.0f), number of stimuli found is %2.0f, expected: %2.0f',idim,idimptr,size(ds{idimptr},1),nstims));
+            wmsg=sprintf('for dim %2.0f (pointer=%2.0f), number of stimuli found is %2.0f, expected: %2.0f',idim,idimptr,size(ds{idimptr},1),nstims);
+            warning(wmsg);
+            opts_used.warnings=strvcat(opts_used.warnings,wmsg);
         end   
         dname=cat(2,'dim',sprintf('%1.0f',idim));
         sout.(dname)=ds{idim};
     else
-        warning(sprintf('entry for dimension pointer %2.0f has no data',idimptr));
+        wmsg=sprintf('entry for dimension pointer %2.0f has no data',idimptr);
+        warning(wmsg);
+        opts_used.warnings=strvcat(opts_used.warnings,wmsg);
     end
 end
 dim_string=deblank(dim_string);
 save(data_fullname,'-struct','sout');
+s_written=sout;
 if opts.if_log
     disp(sprintf('%s written with %2.0f stimuli and dimensions %s.',data_fullname,nstims,dim_string));
 end
